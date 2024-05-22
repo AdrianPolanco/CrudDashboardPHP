3ee2a7a5cde06ea5cb90dfd8115f01b7f5ca181e
Adrián Saul Polanco Ferrer <Adrian>
2024-05-22 13:08:48 -0400

    Database methods refactored

--- data/database.php
+++ data/database.php
@@ -14,17 +14,17 @@ class Database
         $this->connection->set_charset('utf8');
     }
 
-    private function countRecords(): int
+    private function countRecords(string $table = "warriors"): int
     {
-        $sql = "SELECT COUNT(*) FROM warriors";
+        $sql = "SELECT COUNT(*) FROM $table";
         $result = $this->connection->query($sql);
         $row = $result->fetch_assoc();
         return (int)$row["COUNT(*)"];
     }
 
-    public function getRecordById(int $id): ?array
+    public function getRecordById(int $id, string $table = "warriors"): ?array
     {
-        $sql = "SELECT * FROM warriors WHERE id = ?";
+        $sql = "SELECT * FROM $table WHERE id = ?";
         //Usamos prepare para compilar la consulta en la BD y hacerla mas eficiente, pudiendo reutilizarla con diferentes parametros
         //en caso de que se necesite ejecutar la misma consulta con diferentes parametros
         //Adicionalmente, al ejecutar consultas preparadas, automaticamente se escapan los caracteres especiales y los parametros que se enlazan posteriormente son tratados como datos, no como sintaxis de la consulta, evitando inyecciones SQL
@@ -35,10 +35,10 @@ class Database
         return $result->fetch_assoc() ?: null;
     }
 
-    public function getRecordsByPage(int $page): array
+    public function getRecordsByPage(int $page, string $table = "warriors"): array
     {
         $offset = ($page - 1) * 10;
-        $sql = "SELECT * FROM warriors ORDER BY id LIMIT 10 OFFSET ?";
+        $sql = "SELECT * FROM $table ORDER BY id LIMIT 10 OFFSET ?";
         $preparedQuery = $this->connection->prepare($sql);
         $preparedQuery->bind_param("i", $offset);
         $preparedQuery->execute();
@@ -48,17 +48,17 @@ class Database
         return ["data" => $data, "totalPages" => ceil($recordsQuantity / 10)];
     }
 
-    public function insertRecord(string $name, string $lastname, string $birthdate): bool
+    public function insertRecord(string $name, string $lastname, string $birthdate, string $table = "warriors"): bool
     {
-        $sql = "INSERT INTO warriors (nombre, apellido, fecha_nacimiento) VALUES (?, ?, ?)";
+        $sql = "INSERT INTO $table (nombre, apellido, fecha_nacimiento) VALUES (?, ?, ?)";
         $preparedQuery = $this->connection->prepare($sql);
         $preparedQuery->bind_param("sss", $name, $lastname, $birthdate);
         return $preparedQuery->execute();
     }
 
-    public function updateRecord(int $id, string $name, string $lastname, string $birthdate): bool
+    public function updateRecord(int $id, string $name, string $lastname, string $birthdate, string $table = "warriors"): bool
     {
-        $sql = "UPDATE warriors SET nombre = ?, apellido = ?, fecha_nacimiento = ? WHERE id = ?";
+        $sql = "UPDATE $table SET nombre = ?, apellido = ?, fecha_nacimiento = ? WHERE id = ?";
         $preparedQuery = $this->connection->prepare($sql);
         //Asignamos cada una de las letras a cada uno de los valores de las variables que se estan pasando
         //y estas se asignan a los signos de interrogacion, por lo que en lugar del ? estaran los valores de las variables
@@ -67,9 +67,9 @@ class Database
         return $preparedQuery->execute();
     }
 
-    public function deleteRecord(int $id): bool
+    public function deleteRecord(int $id, string $table = "warriors"): bool
     {
-        $sql = "DELETE FROM warriors WHERE id = ?";
+        $sql = "DELETE FROM $table WHERE id = ?";
         $preparedQuery = $this->connection->prepare($sql);
         $preparedQuery->bind_param("i", $id);
         return $preparedQuery->execute();
